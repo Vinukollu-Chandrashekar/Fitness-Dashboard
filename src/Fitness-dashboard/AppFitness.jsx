6907e96062cb1a55cd0fb42b2fe0e6cd5f492abc
Vinukollu Chandrashekar <vinukolluchandrashekar@gmail.com>
2026-01-25 16:38:02 +0530

    Test render AppFitness

--- src/Fitness-dashboard/AppFitness.jsx
+++ src/Fitness-dashboard/AppFitness.jsx
@@ -1,15 +1,23 @@
-import FdMain from "./FD_Main";
-import FitnessNav from "./Fitness_nav";
-import Footer from "./Footer";
+// import FdMain from "./FD_Main";
+// import FitnessNav from "./Fitness_nav";
+// import Footer from "./Footer";
 
-function AppFitness() {
+// function AppFitness() {
+//   return (
+//     <>
+//       <FitnessNav />
+//       <FdMain />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default AppFitness;
+
+export default function AppFitness() {
   return (
-    <>
-      <FitnessNav />
-      <FdMain />
-      <Footer />
-    </>
+    <div style={{ color: "white", padding: "40px" }}>
+      <h1>Fitness Dashboard Loaded ✅</h1>
+    </div>
   );
 }
-
-export default AppFitness;
